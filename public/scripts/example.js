721cbc286ae86536fb0708e7ceb55e1c07ba5bd4
Domenic Murtari <domenic.murtari@gmail.com>
2015-03-09 16:57:36 -0600

    Completed milestone 1

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,13 +1,53 @@
-var CommentBox = React.createClass({
+var converter = new Showdown.converter();
+
+var Comment = React.createClass({
   render: function() {
+    var rawMarkup = converter.makeHtml(this.props.children.toString());
     return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
 });
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
 React.render(
   <CommentBox />,
   document.getElementById('content')
-)
+);
